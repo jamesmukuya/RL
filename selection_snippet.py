96f6cc6ce54a4dfcf9a25fe96ad5fbfad1a558ca
jamesmukuya <james.mukuya@gmail.com>
2019-10-28 12:40:56 +0300

    completed row pairing

--- selection_snippet.py
+++ selection_snippet.py
@@ -11,10 +11,9 @@ import pandas as pd
 
 id = [1,2,3,4,5,6,7,8,9,10,11]
 prev_cost = [18000,20000,50000,30000,65000,14000,10000,30000,35000,40000,12000]
-prev_cost2 = [18000,20000,50000,30000,65000,14000,10000,35000,40000,12000]
 curr_cost = [3300,7000,2000,1000,2500,1500,6000,4000,1300,2000,1000]
-next_cost = [x+y for x, y in zip(prev_cost,curr_cost)]
-next_cost2 = np.add(prev_cost,curr_cost).tolist()
+#next_cost = [x+y for x, y in zip(prev_cost,curr_cost)]
+next_cost = np.add(prev_cost,curr_cost).tolist()
 
 # the highest of prev_cost will be added to the lowest of curr_cost to get best cost
 # 1. arrange the prev_cost in descending order and curr_cost in ascending then add
@@ -24,16 +23,17 @@ sorted_curr_cost = sorted(curr_cost,reverse=False)
 best_cost = np.add(sorted_prev_cost,sorted_curr_cost).tolist()
 #print(best_cost)
 
-# 2. items with an id i.e dataframe
+# 2. items with an id e.g dataframe or database
 
 asc_row_number = [] # row numbers for prev cost from highest to lowest
 desc_row_number = [] # row numbers for curr cost from lowest to highest
 df = pd.DataFrame(list(zip(id,prev_cost,curr_cost)),
-				columns = ['id','prev_cost','curr_cost'])
-#print(df)
+				columns = ['id','prev_cost','curr_cost'],dtype=float)
+#df2 = df.copy()
 next_tot_cost = df['prev_cost'].values + df['curr_cost'].values
 df['next_tot_cost'] = next_tot_cost
-
+df['_prev_cost'] = prev_cost
+df['_curr_cost'] = curr_cost
 # iterate over the dataframe
 for _ in df['prev_cost'].values:	
 	# iterate over the original previous cost list
@@ -44,6 +44,12 @@ for _ in df['prev_cost'].values:
 		row_info_max = df.loc[df['prev_cost'].values==r]
 		# get DataFrame row number of the max value from row info
 		row_numb_max = row_info_max.index.values.astype(int)[0]
+
+		# set the cell data to Nan to prevent any dubplicated row numbers
+		if row_numb_max in asc_row_number:
+			#print(row_numb_max,'already present')
+			df.at[row_numb_max,'prev_cost'] = np.nan
+
 		if r == j:
 			# if value of the row is max, append its number in the container
 			asc_row_number.append(row_numb_max)
@@ -59,38 +65,29 @@ for _ in df['curr_cost'].values:
 		row_info_min = df.loc[df['curr_cost'].values==r]
 		# get DataFrame row number of the max value from row info
 		row_numb_min = row_info_min.index.values.astype(int)[0]
+
+		# set the cell data to Nan to prevent any dubplicated row numbers
+		if row_numb_min in desc_row_number:
+			#print(row_numb_max,'already present')
+			df.at[row_numb_min,'curr_cost'] = np.nan
 		if r == j:
 			# if value of the row is max, append its number in the container
 			desc_row_number.append(row_numb_min)
 			# remove the max number from the previous cost list
 			curr_cost.remove(r)
 
-#print(desc_row_number)
-# get recommended cost column
-recomm_cost = []
-for r_num_min in desc_row_number:
-	#print(r_num_min)
-	curr_cost_row_val = df.iloc[r_num_min].curr_cost
-	recomm_cost.append(curr_cost_row_val)
-	#this val is inserted to the cell of df recomm cost as per index of asc_row_number
-# add this column to the df
+# create an empty column
+df['recomm_cost'] = np.nan
+# for all row numbers in desc and asc row number, insert the value of the min to the row of max
+#df.at[4,'recomm_cost'] = 25000
+x = list(zip(desc_row_number,asc_row_number))
+#print(x[1][0])
+i=0
+for _ in range(len(x)):
+	recomm_val = df.iloc[x[i][0]]._curr_cost
+	df.at[x[i][1],'recomm_cost'] = recomm_val
+	i+=1
+# create col with the best value
+df['sugg_cost'] = df['_prev_cost'].values + df['recomm_cost'].values
+#print(df)
 
-df['recomm_cost'] = recomm_cost
-print(df)
-
-"""
-# get the values of the row numbers from the list
-new_cost = []
-for r_num_max,r_num_min in list(zip(asc_row_number,desc_row_number)):
-	prev_cost_row_value = df.iloc[r_num_max].prev_cost
-	curr_cost_row_value = df.iloc[r_num_min].curr_cost
-	# add the Data Frame values of the lists given by row numbers
-	new_cost.append(np.add(prev_cost_row_value,curr_cost_row_value))
-
-# concatenate this new cost to the df
-df['new_cost'] = new_cost
-print(df)
-"""
-#print(new_cost)
-#print(df.iloc[[3]].prev_cost) # gives a df obj
-#print(df.iloc[3].prev_cost) # gives a series obj
