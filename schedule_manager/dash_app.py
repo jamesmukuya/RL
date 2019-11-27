7bc9021417c217fab9ca87d8816cfa139595ff5e
jamesmukuya <james.mukuya@gmail.com>
2019-11-27 10:22:34 +0300

    created grid for plot, new custom css styles

--- schedule_manager/dash_app.py
+++ schedule_manager/dash_app.py
@@ -1,3 +1,5 @@
+import pandas as pd
+import numpy as np
 #dash imports
 import dash
 import dash_auth
@@ -5,11 +7,20 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Output, Input
 
-import plotly.graph_objects as go
+#import plotly.graph_objects as go
+
+rows=['a','b','c']
+date_range = pd.date_range(start='2019-11-01',end='2019-11-30',freq='H')
+date_list=date_range.tolist() #methods available incl. .time()
+df = pd.DataFrame(index=rows,columns=date_range)
+	#for i in range date_range, if i%60==0: display the hours
+
+date_list=date_range.tolist() #methods available incl. .time()
+
 
 VALID_USERNAME_PASSWORD_PAIRS = [('james','12345'),('susan','12345')]
 # table page size
-page_size = 25
+
 
 #########################
 app = dash.Dash(__name__,assets_folder='static')
@@ -19,110 +30,68 @@ auth = dash_auth.BasicAuth(
     VALID_USERNAME_PASSWORD_PAIRS
 )
 
-app.layout = html.Div([
+app.layout = html.Div(id='main-bg',className='bg-gray-200',children=[
 	html.Div(id='none',children=[],style={'display': 'none'}),
-	html.Div(id='relative-parent',className='relative bg-gray-500',children=[
-		html.Div('date 20rem',id='some-day',className='absolute border border-black border-r-1 border-b-0 border-l-0 pt-0 pb-0 h-screen z-50',
-					style={'width':'96rem','margin-left':'0rem'}),
-		#html.Button('Nexttt',className='absolute ml-2 mt-5 left-0 top-0 bg-gray-200 hover:bg-gray-400 text-gray-800 text-xs font-bold w-16 h-6 border-1 '),
-		html.Div('date2 20rem',id='some-day2',className='absolute border border-black border-r-1 border-b-0 border-l-0 pt-0 pb-0 h-screen z-50',
-					style={'width':'96rem','margin-left':'96rem'}),
+	html.Div(id='relative-parent',className='parent-grid rounded shadow-lg mx-1 bg-gray-500 mt-1 mb-2 border border-black h-full w-full',
+	children=[
+		html.Div(id='equip-nav',className='equip-nav bg-gray-300 px-1 mt-0 border border-black h-full'),
+		html.Div(id='main-plot',className='main-plot bg-blue-500 mt-0 border border-black h-full overflow-scroll overflow-hidden')
 	]),
 	
-	#html.Button('Prev',className='border-2 mx-2'),
-	html.Div(id='btns',className='absolute flex inline-flex border-1 border-black mt-6',children=[
-		html.Button('Prev',className='bg-blue-300 hover:bg-gray-400 text-gray-800 text-xs font-bold h-6 border border-black border-1 ',
-						style={'width':'4rem'}),
-		html.Button('Next',className='bg-gray-200 hover:bg-gray-400 text-gray-800 text-xs font-bold w-16 h-6 border border-black border-1 '),
-		html.Button('Also Next',className='bg-gray-200 hover:bg-gray-400 text-gray-800 text-xs font-bold w-16 h-6 border border-black border-1 '),
-		#html.Div(id='some-date',className='border border-black border-l-1 pt-0 pb-0'),
-		html.Button('After div',className='bg-gray-200 hover:bg-gray-400 text-gray-800 text-xs font-bold w-16 h-6 border border-black border-1 '),
-		html.Button('After div',className='bg-gray-200 hover:bg-gray-400 text-gray-800 text-xs font-bold w-16 h-6 border border-black border-1 '),
-	]),
-		html.Button('flight one',className='bg-green-400 hover:bg-gray-400 text-gray-800 text-xs font-bold h-8 border border-black border-1',
-					style={'width':'8rem','margin-left':'16rem','margin-top':'12rem'})
 ])
 
-"""
-y=[1,1]
-@app.callback(Output('sample-plot','figure'),
-			[Input('none','children')])
-def plot_bar(somebar):
-	traces=[]
-	layouts=[]
-	charts = [go.Scatter(
-					x=[1,2],
-					y=y,
-					fill='tonexty',
-					fillcolor='darkviolet',
-					#orientation='h',
-					mode='lines',
-					#stackgroup='one',
-					marker={
-						'line':{
-							'width':1.0,
-								}
-							}
-						),
-				go.Scatter(
-					x=[3,6],
-					y=y,
-					fill='tonexty',
-					fillcolor = 'violet',
-					#orientation='h',
-					mode='lines',
-					stackgroup='one',
-					marker={
-						'line':{
-							'width':1.0,
-								}
-							}
-						),
-				go.Scatter(
-					x=[5,38],
-					y=y,
-					mode='lines',
-					line={
-						'width':50
-					},
-					#stackgroup='z',
-					marker={
-						'line':{
-							'width':1.0,
-								}
-							}
-						),
-				dcc.Checklist(id='chk-in-graph',options=[{
-					'label':'gA', 'value':'fafa'
-				}],
-				value=['fafa'],)
-		]
-	for chart in charts:
-		traces.append(chart)
+#print(df.index)
+# plot the schedule from database on loading
+@app.callback(Output('equip-nav','children'),
+[Input('none','children')])
+def insert_equip(tail):
+	tails=['','5a','5b','5c','5d','5e']
+	btns=[]
+	# looping over planned sched
+	for btn in tails:
+		btns.append(html.Div(html.Button(btn,id=btn)))
+	# adding using single event
+	btns.append(html.Div(html.Button('5f')))
+	return btns
 
-	layout=go.Layout(
-			#autosize=True,
-			height=300,
-			shapes=[
-				go.layout.Shape(
-					type='line',
-					xref="x",yref="paper",
-					x0=10,x1=10,y0=0,y1=1, layer='above',
-					line_width=2
-				)
-			]
-		)
+@app.callback(Output('main-plot','children'),
+[Input('none','children')])
+def insert_flight(flight):
+	btns=[]
+	time_btn_classes = 'timebtn'
+	begin=1
+	for date_hour in date_list:
+		init_time = begin
+		end_time = 60 + init_time
+		btns.append(html.Button(str(date_hour),className=time_btn_classes,
+					style={'grid-column-start':str(init_time),'grid-column-end':str(end_time),
+							'grid-row-start':'1'}))
+		begin = end_time
+	# using single call events like button click	
+	btns.append(html.Button('new flight',className='h-8 bg-green-400 border border-blue-700 rounded',
+		style={'grid-column-start':'7','grid-column-end':'65', 'grid-row-start':'3'}))
+	btns.append(html.Button('new flight',className='h-8 bg-green-400 border border-blue-700 rounded',
+		style={'grid-column-start':'50','grid-column-end':'95', 'grid-row-start':'3'}))
+	btns.append(html.Button('new flight',className='h-8 bg-green-400 border border-blue-700 rounded',
+		style={'grid-column-start':'50','grid-column-end':'105', 'grid-row-start':'2'}))
+	return btns
 
-	return {
-		'data':traces,
-		'layout':layout,
-	}
-"""
+
+
+
+
+
+
+
+
+
+
+
+
+
 def main():
 	app.run_server(debug=True)
-
-
-
+	pass
 
 if __name__ == '__main__':
     main()
